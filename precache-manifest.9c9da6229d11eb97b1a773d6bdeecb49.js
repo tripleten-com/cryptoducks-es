316c9e686d3f99584978c2e9277f16716f05ad67
arty gvozdenkov <gvozdenkov@gmail.com>
2022-10-17 19:17:21 +0300

    Updates

---
+++ precache-manifest.9c9da6229d11eb97b1a773d6bdeecb49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b66e8d473a64268118971f525b00c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9e36d54afe4831e795c",
+    "url": "./static/css/main.bcc24b2c.chunk.css"
+  },
+  {
+    "revision": "33e5e295b0b2669ab204",
+    "url": "./static/js/2.0b03fabd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.0b03fabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e36d54afe4831e795c",
+    "url": "./static/js/main.c9fc30e6.chunk.js"
+  },
+  {
+    "revision": "5df3ff523e8bd298e93e",
+    "url": "./static/js/runtime-main.b2b7aeb4.js"
+  }
+]);
